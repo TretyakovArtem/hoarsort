60fa34141fc80933171156d880cd2d790df82e33
pathfinder <artem.tretyakov.91@inbox.ru>
2020-04-12 15:58:16 +0300

    доработка визуализатора

--- hoar.py
+++ hoar.py
@@ -1,6 +1,12 @@
 from tkinter import *
+from random import randrange
 
 root = Tk()
+
+BX_BEGIN = 360
+BY_BEGIN = 10
+EX_BEGIN = 390
+EY_BEGIN = 40
 
 # слева разместим холст, на котором и будет визуализация
 c = Canvas(width=900, height=700, bg='white')
@@ -15,15 +21,44 @@ class Input:
         self.i.grid(row=row, column=column)
 
 class ManageButton:
-    def __init__(self, master, text, row, column, cspan, sticky):
-        self.b = Button(text=text)
+    def __init__(self, master, text, row, column, cspan, sticky, command=None):
+        self.b = Button(text=text, command=command)
         self.b.grid(row=row, column=column, columnspan=cspan, sticky=sticky, padx=5)
 
+cells = []
+values = []
+
 for i in range(6):
-    Input(root, 0, i+1)
- 
-loadb = ManageButton(root, "Загрузить", row=1, column=1, cspan=3, sticky=W)
-randomb = ManageButton(root, "Сгенерировать", row=1, column=4, cspan=3, sticky=E)
+    cells.append(Input(root, 0, i+1))
+
+
+def get_values():
+    values = []
+    for cell in cells:
+        values.append(int(cell.i.get()))
+    
+    c.delete("all")
+    first = OperationalArray(values, BX_BEGIN, BY_BEGIN, EX_BEGIN, EY_BEGIN, 0)
+    first.returnArrow(first.merged, 550, 25, 580, 25)
+    print(values)
+
+
+def generate():
+    values = []
+    for cell in cells:
+        num = randrange(10)
+        values.append(num)
+        cell.i.delete(0)
+        cell.i.insert(0,num)
+    
+    c.delete("all")
+    first = OperationalArray(values, BX_BEGIN, BY_BEGIN, EX_BEGIN, EY_BEGIN, 0)
+    first.returnArrow(first.merged, 550, 25, 580, 25)
+    print(values)
+
+
+loadb = ManageButton(root, "Загрузить", row=1, column=1, cspan=3, sticky=W, command=get_values)
+randomb = ManageButton(root, "Сгенерировать", row=1, column=4, cspan=3, sticky=E, command=generate)
 
 startb = ManageButton(root, "Старт", row=2, column=1, cspan=3, sticky=W)
 stopb = ManageButton(root, "Сброс", row=2, column=4, cspan=3, sticky=W)
@@ -32,11 +67,15 @@ forwardb = ManageButton(root, "Вперед", row=3, column=5, cspan=2, sticky=E
 reversb = ManageButton(root, "Назад", row=3, column=1, cspan=2, sticky=W)
 pauseb = ManageButton(root, "Пауза", row=3, column=3, cspan=2, sticky=None)
 
+
+
 # опорный элемент выбираем всегда первый
 class OperationalArray:
-    def __init__(self, arr, bx, by, ex, ey):
+    def __init__(self, arr, bx, by, ex, ey, level):
+        self.level = level
         self.build(arr, bx, by, ex, ey)
         self.merged = self.divide(arr, bx, by, ex, ey)
+        
     
     def build(self, arr, bx, by, ex, ey):
         pivot = 'orange'
@@ -69,37 +108,50 @@ class OperationalArray:
         # отрисовать опорный элемент
         if len(arr) < 2:
             return arr
-        c.create_text(bx+90, by+60, text=arr[0], font="Verdana 14")
+        c.create_text((bx + (len(arr) / 2) * 30), by+60, text=arr[0], font="Verdana 14")
 
         less = [i for i in arr[1:] if i <= arr[0]]
         greater = [i for i in arr[1:] if i > arr[0]]
 
-        bx_l = bx - 30
+        if self.level == 0:
+            self.level = 2
+        else:
+            self.level = self.level * 2
+
+        # сдивигаем меньший массив
+        bx_l = bx - (BX_BEGIN // self.level)
         by_l = by + 45
         ex_l = bx_l + 30
         ey_l = by_l + 30
 
+        one = None
+        if len(less) == 1:
+            one = "green"
+
         for i in less:
-            c.create_rectangle(bx_l, by_l, ex_l, ey_l)
+            c.create_rectangle(bx_l, by_l, ex_l, ey_l, outline=one)
             c.create_text(bx_l+15, by_l+15, text=i, font="Verdana 14")
             pivot = None
             bx_l += 30
             ex_l += 30
 
-        bx = bx + 90 + 30
-        by = by + 45
+        bx_g = (bx +  (BX_BEGIN // self.level))
+        by_g = by + 45
 
-        ex = bx + 30
-        ey = by + 30
+        ex_g = bx_g + 30
+        ey_g = by_g + 30
+
+        one = None
+        if len(greater) == 1:
+            one = "green"
 
         for i in greater:
-            c.create_rectangle(bx, by, ex, ey)
-            c.create_text(bx+15, by+15, text=i, font="Verdana 14")
-            bx += 30
-            ex += 30
-
+            c.create_rectangle(bx_g, by_g, ex_g, ey_g, outline=one)
+            c.create_text(bx_g+15, by_g+15, text=i, font="Verdana 14")
+            bx_g += 30
+            ex_g += 30
         
-        return OperationalArray(less, bx-300, by+60, ex-300, ey+60).merged + [arr[0]] + OperationalArray(greater, bx+50, by+60, ex+50, ey+60).merged
+        return OperationalArray(less, (BX_BEGIN // self.level), by_l+60, (BX_BEGIN // self.level) + 30, ey_l+60, self.level).merged + [arr[0]] + OperationalArray(greater, (bx +  (bx // 2)), by_g+60, (bx +  (bx // 2)) + 30, ey_g+60, self.level).merged
         
 
 
@@ -107,16 +159,12 @@ class OperationalArray:
 # после этого нужно показать на первый (опорный элемент)
 # и то как массив делится на два
 
+sortArray = [4, 1, 2, 5, 3, 6]
 
-bx = 360
-by = 10
-ex = 390
-ey = 40
-
-sortArray = [4, 6, 1, 5, 3, 2]
-
-first = OperationalArray(sortArray, bx, by, ex, ey)
+first = OperationalArray(sortArray, BX_BEGIN, BY_BEGIN, EX_BEGIN, EY_BEGIN, 0)
 first.returnArrow(first.merged, 550, 25, 580, 25)
+
+
 
 # теперь нужно сделать отступ вниз и отобразить там опорный элемент
 # слева от него отобразить массив с меньшими значениями а справа - с большими
---
+++ quicksort.py
@@ -0,0 +1,9 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        grater = [i for i in array[1:] if > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
